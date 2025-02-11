9701600201560a79d66418cc1b6aabdf4fc6678f
Shiva <shivatejakura00@gmail.com>
2025-02-11 22:16:05 +0530

    Added OrderDto,UserDto and Order.cs

---
+++ Client/Dtos/OrderDto.cs
@@ -0,0 +1,15 @@
+﻿using PickleInventory.Domain.Entities;
+
+
+namespace PickleInventory.Client.Dtos
+{
+    public record OrderDto
+        (int OrderId,
+        string UserId, 
+        string UserName,
+        string UserAddress,
+        DateTime OrderDate,
+        List<OrderItemDto> OrderItems,
+        decimal TotalAmount,
+        DeliveryStatus DeliveryStatus);
+}
